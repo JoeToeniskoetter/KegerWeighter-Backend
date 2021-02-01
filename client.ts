f203ad9d3c4565d53e8b06dc230e83eb4921e203
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2021-02-01 17:48:52 -0600

    updated client version for testing

--- client.ts
+++ client.ts
@@ -1,12 +1,77 @@
 import * as io from "socket.io-client";
 import { KegEvents, KegUpdate } from "./src/shared/types";
-// ?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiZW1haWxAZW1haWwuY29tIiwiaWQiOiI2MTgzNzM2ZS1lZmM0LTQwYzUtOWQxYi1iMTliODlhNzZjNGMifSwiaWF0IjoxNjA2ODczNzYxLCJleHAiOjE2MDY5NjAxNjF9.oZ_QU-lWpR68Yrm6Izgfy2D3CCZgoC91KO8W75cIjHc"
 
-const client = io(
-  "http://localhost:3000/user/feed?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImlkIjoiNDQ3MTJjYjEtOTYyMS00MDIxLWJlMWEtNDdmYmY5ZWE3YmZjIn0sImlhdCI6MTYwNzUyNTczMiwiZXhwIjoxNjA3NjEyMTMyfQ.-144FK8HlhxNjU02ZCT3WNzY44tAr0bOnbioAefzDa8"
-);
+const client = io("http://localhost:3000/?x-keg-token=username:password", {
+  forceNew: true,
+});
 client.on("connect", () => {
   console.log("connected");
+  setInterval(() => {
+    let weight = Math.random() * (165 - 30) + 30;
+    let temp = Math.random() * (32 - 50) + 32;
+    console.log("sending data");
+    client.emit(KegEvents.UPDATE, {
+      weight,
+      temp,
+    });
+  }, 4500);
+});
+
+const client2 = io(
+  "http://localhost:3000/?x-keg-token=3FE8D718-21F7-C8F7-D3D3-A9EE26F69C58:password",
+  {
+    forceNew: true,
+  }
+);
+
+client2.on("connect", () => {
+  console.log("connected");
+  setInterval(() => {
+    let weight = Math.random() * (165 - 30) + 30;
+    let temp = Math.random() * (32 - 50) + 32;
+    console.log("sending data");
+    client2.emit(KegEvents.UPDATE, {
+      weight,
+      temp,
+    });
+  }, 2000);
+});
+
+const client3 = io(
+  "http://localhost:3000/?x-keg-token=ef9c6a82-7b13-42d6-a898-a8eb0be797e7:password",
+  {
+    forceNew: true,
+  }
+);
+
+client3.on("connect", () => {
+  console.log("connected");
+  setInterval(() => {
+    let weight = Math.random() * (165 - 30) + 30;
+    let temp = Math.random() * (32 - 50) + 32;
+    console.log("sending data");
+    client3.emit(KegEvents.UPDATE, {
+      weight,
+      temp,
+    });
+  }, 3000);
+});
+
+const client4 = io("http://localhost:3000/?x-keg-token=test:password", {
+  forceNew: true,
+});
+
+client4.on("connect", () => {
+  console.log("connected");
+  setInterval(() => {
+    let weight = Math.random() * (165 - 30) + 30;
+    let temp = Math.random() * (32 - 50) + 32;
+    console.log("sending data");
+    client4.emit(KegEvents.UPDATE, {
+      weight,
+      temp,
+    });
+  }, 2500);
 });
 
 client.on(KegEvents.UPDATE, (data: any) => {
@@ -25,8 +90,8 @@ client.on("disconnect", () => {
   console.log("disconnected");
 });
 client.on("connect_error", (e: any) => {
-  console.log(e);
+  console.log(e.message);
 });
 client.on("connect_failed", (e: any) => {
-  console.log(e);
+  console.log(e.message);
 });

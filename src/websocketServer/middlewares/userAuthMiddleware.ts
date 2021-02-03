cb7964d097bffb751226e0a03d4480750a238291
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2021-02-02 19:19:56 -0600

    trying to fix errors with invalid signatures

--- src/websocketServer/middlewares/userAuthMiddleware.ts
+++ src/websocketServer/middlewares/userAuthMiddleware.ts
@@ -1,10 +1,11 @@
 import * as jwt from "jsonwebtoken";
+require("dotenv").config();
 
 export async function checkSocketTokens(socket: any, next: any) {
   let token = socket.handshake.query["x-auth-token"];
   if (token) {
     try {
-      jwt.verify(token, process.env.JWT_SECRET || "");
+      jwt.verify(token, process.env.JWT_SECRET);
       let decodeduser: any = jwt.decode(token);
       socket.user = decodeduser.user;
       await socket.join(decodeduser.id);
